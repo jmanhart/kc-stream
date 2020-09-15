9069ab6dd7ed8f2cf9ce1e1dde01af30e09e6ffc
jmanhart <jmanhart@sentry.io>
2020-09-15 17:41:01 -0500

    Splitting out the data from the component

--- src/App.js
+++ src/App.js
@@ -1,138 +1,15 @@
 import React from 'react';
 import './App.scss';
+import StreamerData from './streamer-data.js';
 
-
-
-const data = [
-  {
-    "name": "ArtificialAndrew",
-    "link": "https://www.twitch.tv/artificialandrew",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "BZHub",
-    "link": "https://www.twitch.tv/BZHub",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "chachitownfair",
-    "link": "https://www.twitch.tv/chachitownfair",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "denec27",
-    "link": "Https://www.twitch.tv/denec27",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "gibrias",
-    "link": "https://www.twitch.tv/gibrias",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "HusKerrs",
-    "link": "https://www.twitch.tv/huskerrs",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "hinzae",
-    "link": "https://www.twitch.tv/hinzae",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "HYPRRR",
-    "link": "https://www.facebook.com/HYPRRR/",
-    "platform": "Facebook",
-    "platformImg": "FbLogoColor.png"
-  },
-  {
-    "name": "john_manhart",
-    "link": "https://www.twitch.tv/john_manhart",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "kooglertv",
-    "link": "https://www.twitch.tv/kooglertv",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "lotis_mindset",
-    "link": "https://www.twitch.tv/lotis_mindset",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "MnMenace86",
-    "link": "https://www.facebook.com/MnMenace86/",
-    "platform": "Facebook",
-    "platformImg": "FbLogoColor.png"
-  },
-  {
-    "name": "Nerdmeat_",
-    "link": "https://www.twitch.tv/nerdmeat_",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "RTY_KC",
-    "link": "https://www.twitch.tv/RTY_KC",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "sinrin",
-    "link": "https://www.twitch.tv/sinrin",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "slamdunkpony",
-    "link": "https://www.twitch.tv/slamdunkpony",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "SovereignSyd",
-    "link": "https://www.twitch.tv/sovereignsyd",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "starsofnight",
-    "link": "https://www.twitch.tv/starsofnight",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "tegannotsara1",
-    "link": "https://www.twitch.tv/tegannotsara1",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  },
-  {
-    "name": "yantzi",
-    "link": "https://www.twitch.tv/yantzi",
-    "platform": "Twitch",
-    "platformImg": "TwitchGlitchPurple.png"
-  }
-]
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h2>🎮 Kansas City Streamers</h2>
-      <p>Last updated 6/10/2020 | <a href="https://twitter.com/JohnManhart">Ask to Join</a></p>
+      <p>Last updated 9/15/2020 | <a href="https://twitter.com/JohnManhart">Ask to Join</a></p>
       <ul>
-        {data.map((item) => {
+        {StreamerData.map((item) => {
           return (
             <li>
               <a
---
+++ src/streamer-data.js
@@ -0,0 +1,124 @@
+const StreamerData = [
+    {
+      "name": "ArtificialAndrew",
+      "link": "https://www.twitch.tv/artificialandrew",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "BZHub",
+      "link": "https://www.twitch.tv/BZHub",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "chachitownfair",
+      "link": "https://www.twitch.tv/chachitownfair",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "denec27",
+      "link": "Https://www.twitch.tv/denec27",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "gibrias",
+      "link": "https://www.twitch.tv/gibrias",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+        "name": "hinzae",
+        "link": "https://www.twitch.tv/hinzae",
+        "platform": "Twitch",
+        "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "HusKerrs",
+      "link": "https://www.twitch.tv/huskerrs",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "HYPRRR",
+      "link": "https://www.facebook.com/HYPRRR/",
+      "platform": "Facebook",
+      "platformImg": "FbLogoColor.png"
+    },
+    {
+      "name": "ItsManhart",
+      "link": "https://www.twitch.tv/ItsManhart",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "kooglertv",
+      "link": "https://www.twitch.tv/kooglertv",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "lotis_mindset",
+      "link": "https://www.twitch.tv/lotis_mindset",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "MnMenace86",
+      "link": "https://www.facebook.com/MnMenace86/",
+      "platform": "Facebook",
+      "platformImg": "FbLogoColor.png"
+    },
+    {
+      "name": "Nerdmeat_",
+      "link": "https://www.twitch.tv/nerdmeat_",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "RTY_KC",
+      "link": "https://www.twitch.tv/RTY_KC",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "sinrin",
+      "link": "https://www.twitch.tv/sinrin",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "slamdunkpony",
+      "link": "https://www.twitch.tv/slamdunkpony",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "SovereignSyd",
+      "link": "https://www.twitch.tv/sovereignsyd",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "starsofnight",
+      "link": "https://www.twitch.tv/starsofnight",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "tegannotsara1",
+      "link": "https://www.twitch.tv/tegannotsara1",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    },
+    {
+      "name": "yantzi",
+      "link": "https://www.twitch.tv/yantzi",
+      "platform": "Twitch",
+      "platformImg": "TwitchGlitchPurple.png"
+    }
+  ]
+
+  export default StreamerData;
